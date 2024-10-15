d3ec9a19a0a2f43e32039adba79b15f87cb7fdf8
Lan Laucirica <lan@primedeviation.com>
2024-10-15 10:10:36 -0700

    onboarding

--- pushpull.sh
+++ pushpull.sh
@@ -24,10 +24,10 @@ sleep 60
 # Download the zip file from GitHub
 curl -L -o mentus-extension.zip https://github.com/PrimeDeviation/mentus/blob/paywall/mentus-extension.zip?raw=true
 
+cp mentus-extension.zip -d /mnt/c/localcode
+
 # Delete the contents of the specified directory before extraction
 rm -rf /mnt/c/localcode/mentus-extension-o1/*
-
-cp mentus-extension.zip -d /mnt/c/localcode
 
 # Extract the zip file to the specified directory
 unzip mentus-extension.zip -d /mnt/c/localcode/mentus-extension-o1
